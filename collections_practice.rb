c835d2a38d4cc26c09e0d95e3b632788ca35c8dc
George Gore <georgelgore@gmail.com>
2017-10-03 21:49:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,8 @@ def merge_data(keys, data)
     keys.each do |hash|
       # final_array << hash.merge!(data_hash)# final_array << {hash, data_hash}
       if hash.keys == hash1.keys
-      puts hash.merge!(hash1.keys)
+        puts hash.merge!(hash1.keys)
+      end
     end
   end
   final_array
